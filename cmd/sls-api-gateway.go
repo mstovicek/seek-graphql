c13cd7f22530ef43847e4fefe5f0e1897f0f739d
Milan Stovicek <stovicek.milan@gmail.com>
2018-04-09 15:16:22 +0200

    adds cors

--- cmd/sls-api-gateway.go
+++ cmd/sls-api-gateway.go
@@ -7,9 +7,23 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/mstovicek/seek-2/schema"
 	"encoding/json"
+	"github.com/caarlos0/env"
 )
 
+type config struct {
+	CorsAllowOrigin string `env:"CORS_ALLOW_ORIGIN" envDefault:"*"`
+	CorsAllowMethods string `env:"CORS_ALLOW_METHODS" envDefault:"*"`
+}
+
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	cfg := config{}
+	err := env.Parse(&cfg)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+		}, err
+	}
+
 	log.Printf("request: %v, body: %s, context: %v", request, request.Body)
 	response, _ := schema.Execute(ctx, request.Body)
 
@@ -22,6 +36,10 @@ func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 
 	return events.APIGatewayProxyResponse{
 		Body:       string(rJSON),
+		Headers: map[string]string {
+			"Access-Control-Allow-Origin": cfg.CorsAllowOrigin,
+			"Access-Control-Allow-Methods": cfg.CorsAllowMethods,
+		},
 		StatusCode: 200,
 	}, nil
 }
