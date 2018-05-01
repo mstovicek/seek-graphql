e8a08021650ec5fa312e2f87980904a87932e0c4
Milan Stovicek <stovicek.milan@gmail.com>
2018-05-01 12:49:39 +0200

    add test me - category - card

--- cmd/respose_test.go
+++ cmd/respose_test.go
@@ -47,7 +47,8 @@ func TestMeWithCategory(t *testing.T) {
 			edges{
 				cursor,
 				node {
-					id
+					id,
+					title
 				}
 			},
 			pageInfo{
@@ -71,7 +72,97 @@ func TestMeWithCategory(t *testing.T) {
 					{
 						"cursor": "cursor:0",
 						"node": {
-							"id": "0"
+							"id": "0",
+							"title": "category@0"
+						}
+					}
+				],
+				"pageInfo": {
+					"startCursor": "cursor:0",
+					"endCursor": "cursor:0",
+					"hasNextPage": true
+				}
+			}
+		}
+	}
+}`
+
+	actualResponse, err := getResponse(query)
+
+	assert.Nil(t, err)
+	assertEqualResponse(t, expectedResponse, *actualResponse)
+}
+
+func TestMeWithCategoryAndCard(t *testing.T) {
+	query := `query {
+	me {
+		id,
+		email,
+		name,
+		categories (first: 1) {
+			totalCount,
+			edges{
+				cursor,
+				node {
+					id,
+					title,
+					cards (first: 1) {
+						totalCount,
+						edges {
+							cursor,
+							node {
+								id,
+								title
+							}
+						}
+						pageInfo{
+							startCursor,
+							endCursor,
+							hasNextPage
+						},
+					}
+				}
+			},
+			pageInfo{
+				startCursor,
+				endCursor,
+				hasNextPage
+			}
+		}
+	}
+}`
+
+	expectedResponse := `{
+	"data": {
+		"me": {
+			"id": "42",
+			"email": "milan@me",
+			"name": "Milan",
+			"categories": {
+				"totalCount": 999,
+				"edges": [
+					{
+						"cursor": "cursor:0",
+						"node": {
+							"id": "0",
+							"title": "category@0",
+							"cards": {
+								"totalCount": 999,
+								"edges": [
+									{
+										"cursor": "cursor:0",
+										"node": {
+											"id": "0",
+											"title": "category@0:card@0"
+										}
+									}
+								],
+								"pageInfo": {
+									"startCursor": "cursor:0",
+									"endCursor": "cursor:0",
+									"hasNextPage": true
+								}
+							}
 						}
 					}
 				],
